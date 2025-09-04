aa6d195d55ff2241cec8fe50f25aa07e4e684cfc
mrsj101 <sergiojacob.vp@gmail.com>
2025-09-04 07:59:09 +0300

    refactor: centralize firebase config

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,21 +1,13 @@
-importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js');
+import { initializeApp } from "firebase/app";
+import { getMessaging, onBackgroundMessage } from "firebase/messaging/sw";
+import { firebaseConfig } from "../src/integrations/firebase/config";
 
-firebase.initializeApp({
-  apiKey: "AIzaSyB_bJG2fVt-w5lRJ8BhTqY8Xzm2sGpxl98",
-  authDomain: "barbertor-5625d.firebaseapp.com",
-  projectId: "barbertor-5625d",
-  storageBucket: "barbertor-5625d.firebasestorage.app",
-  messagingSenderId: "891297825422",
-  appId: "1:891297825422:web:f7af30210af25a8a12bfdf",
-  measurementId: "G-7KKTVEM6JP",
-});
+const app = initializeApp(firebaseConfig);
+const messaging = getMessaging(app);
 
-const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage((payload) => {
-  self.registration.showNotification(payload.notification?.title ?? '', {
+onBackgroundMessage(messaging, (payload) => {
+  self.registration.showNotification(payload.notification?.title ?? "", {
     body: payload.notification?.body,
-    icon: '/icon-192.png',
+    icon: "/icon-192.png",
   });
 });
